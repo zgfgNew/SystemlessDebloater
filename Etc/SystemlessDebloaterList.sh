c8c94424c018ac341c9583785dbfb4b2a1275a5c
zgfgNew <ivo.penzar+PaySafeCard-test@soar.hr>
2025-07-28 12:59:52 +0200

    Add files via upload

---
+++ Etc/SystemlessDebloaterList.sh
@@ -0,0 +1,21 @@
+# Input debloat list /storage/emulated/0/Download/SystemlessDebloaterList.sh for Magisk Module Systemless Debloater (REPLACE) v1.4.0
+# Before debloating the apps, from Settings/Applications, Uninstall (updates) and Clear Data!
+# Systemless Debloater log: /storage/emulated/0/Download/SystemlessDebloater.log
+# Copyright (c) zgfg @ xda, 2020-2021
+ 
+VerboseLog="true"
+MultiDebloat=""
+
+# SAR mount-points (system partitions to search for debloating)
+SarMountPointList="
+/system
+/odm
+/product 
+/vendor 
+/system_ext"
+
+# Define a list of stock apps for debloating:
+
+# Need for Android Auto:
+#AndroidAutoStub
+#Velvet
